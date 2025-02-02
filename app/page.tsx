edd08292a7f112ebfd341f6eae1648f4d9fe19e9
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-02 22:10:53 +0300

    Move GitHub data fetching to layout for persistence across sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default async function Page() {
-  const res = await fetch("https://api.github.com/repos/vercel/next.js");
-  const data = await res.json();
   return (
     <div className="flex justify-center">
-      <h1 className="text-[30px]">{data.id}</h1>
+      <h1 className="text-[25px]">hello</h1>
     </div>
   );
 }

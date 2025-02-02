4cedb543253fabfc8601e12a0647e8da3cadae81
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-02 21:48:21 +0300

    Fetch and display GitHub repo data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default async function Page() {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const data = await res.json();
   return (
     <div className="flex justify-center">
-      <h1 className="text-[30px]">hello</h1>
+      <h1 className="text-[30px]">{data.id}</h1>
     </div>
   );
 }
